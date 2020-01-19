c6d3c72ca00d4c4c01d0016088303c5028e4b70a
Amit Kumar <amit@joshsoftware.com>
2020-01-19 22:48:25 +0530

    update package version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 import UploadToS3 from "./UploadToS3";
 
 function App() {
